b6dd6fd7ee8a12bae7ad4b8405971997de75001d
Gerard <kanetpwnz@gmail.com>
2022-11-23 20:38:07 +0100

    repo creation

---
+++ script.js
@@ -0,0 +1,19 @@
+// 16x16 grid
+// hover effect so grid divs change color leaving trail thru grid
+// prompt asking squares for new grid, from 16x16 to 100x100 > always inside same size
+//
+// document.getElementById(`class`).innerHTML = variable;
+
+let container = document.querySelector(`.container`);
+
+container.forEach((div) => {
+  div.addEventListener('click', () => {
+    // code
+  });
+});
+
+container.forEach((div) => {
+  div.addEventListener('hoover', () => {
+    // code
+  });
+});
