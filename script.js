33d30173a33e46667957d7e08b1994bdb3364b72
Gerard <kanetpwnz@gmail.com>
2022-12-12 15:57:19 +0100

    style finish

--- script.js
+++ script.js
@@ -6,32 +6,42 @@ let multi = document.getElementById(`multi`).addEventListener(`click`, multiply)
 let divid = document.getElementById(`division`).addEventListener(`click`, divide);
 let igual = document.getElementById(`igual`).addEventListener(`click`, operate);
 
+let uno = document.getElementById(`uno`).addEventListener(`click`, );
+let dos = document.getElementById(`dos`).addEventListener(`click`, );
+let tres = document.getElementById(`tres`).addEventListener(`click`, );
+let cuatro = document.getElementById(`cuatro`).addEventListener(`click`, );
+let cinco = document.getElementById(`cinco`).addEventListener(`click`, );
+let seis = document.getElementById(`seis`).addEventListener(`click`, );
+let siete = document.getElementById(`siete`).addEventListener(`click`, );
+let ocho = document.getElementById(`ocho`).addEventListener(`click`, );
+let nueve = document.getElementById(`nueve`).addEventListener(`click`, );
+
 // add, substract, multiply, divide
 
 function add(a, b) {
-
+    document.getElementsByClassName('pantalla').innerHTML = "";
 }
 
 function substract(a, b) {
-  
+    document.getElementsByClassName('pantalla').innerHTML = "";
 }
 
 function multiply(a, b) {
-  
+    document.getElementsByClassName('pantalla').innerHTML = "";
 }
 
 function divide(a, b) {
-  
+    document.getElementsByClassName('pantalla').innerHTML = "";
 }
 
 // operate (takes operator and 2 numbers and calls function above)
 
 function operate (a, b) {
-
+    document.getElementsByClassName('pantalla').innerHTML = "";
 }
 
 // storing display value
 
 function screenvalue() {
-
+    document.getElementsByClassName('pantalla').innerHTML = "";
 }
