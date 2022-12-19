b4cd3a9b8775648a29d85ad3dd9b624eeee296ea
Gerard <kanetpwnz@gmail.com>
2022-12-19 09:05:43 +0100

    finish coding

--- script.js
+++ script.js
@@ -1,66 +1,262 @@
-// buttons listeners #decimales
+// variables
 
-let sumar = document.getElementById(`sumar`).addEventListener(`click`, add);
-let restar = document.getElementById(`restar`).addEventListener(`click`, substract);
-let multi = document.getElementById(`multi`).addEventListener(`click`, multiply);
-let divid = document.getElementById(`division`).addEventListener(`click`, divide);
-let igual = document.getElementById(`igual`).addEventListener(`click`, operate);
-let numerosbtn = document.querySelectorAll(".botoniguales");
-let pantallaUNO = document.getElementById('spanscreen');
+let AC = document.getElementById("ce").addEventListener(`click`, ce);
+let C = document.getElementById("c").addEventListener("click", ce);
+let masmenos = document.getElementById("masmenos").addEventListener("click", buttonNegativeClicked);
+let sumar = document.getElementById(`sumar`).addEventListener("click", buttonAddClicked);
+let restar = document.getElementById(`restar`).addEventListener("click", buttonSubtractClicked);
+let multi = document.getElementById(`multi`).addEventListener("click", buttonMultiplyClicked);
+let divid = document.getElementById(`division`).addEventListener("click", buttonDivideClicked);
+let igual = document.getElementById(`igual`).addEventListener("click", buttonEqualsClicked);
+let buttonPoint = document.getElementById("decimales").addEventListener("click", buttonPointClicked);
+let button1 = document.getElementById("uno").addEventListener("click", button1Clicked);
+let button2 = document.getElementById("dos").addEventListener("click", button2Clicked);
+let button3 = document.getElementById("tres").addEventListener("click", button3Clicked);
+let button4 = document.getElementById("cuatro").addEventListener("click", button4Clicked);
+let button5 = document.getElementById("cinco").addEventListener("click", button5Clicked);
+let button6 = document.getElementById("seis").addEventListener("click", button6Clicked);
+let button7 = document.getElementById("siete").addEventListener("click", button7Clicked);
+let button8 = document.getElementById("ocho").addEventListener("click", button8Clicked);
+let button9 = document.getElementById("nueve").addEventListener("click", button9Clicked);
+let button0 = document.getElementById("zero").addEventListener("click", button0Clicked);
+let pantallaTop = document.getElementById("pantallaTop");
+let pantallaBottom = document.getElementById("pantallaBottom");
+pantallaTop.textContent = "";
+pantallaBottom.textContent = "";
+let defaultBottomNumber = 0;
+let number1 = "";
+let number2 = "";
+let number3 = "";
+let operatorClicked = false;
+let operator;
 
-// pantalla
+// operaciones
 
-pantallaUNO.innerHTML = "0";
-
-numerosbtn.forEach((num) => {
-    num.addEventListener("click", actnumeros);
-});
-
-function actnumeros(e) {
-    let numVal; // Store the value of the number
-        if (e.type === "click") {
-          numVal = e.target.innerHTML;
-          pantallaUNO.innerHTML = numVal;
-        } else {
-          return;
-        }
+function add(number1, number2){
+  return number1 + number2;
 }
 
-/* crear 2 pantallas virtuales para poder operar entre ellas
-
-pasar a numero el innerhtml de las pantallas para computar:
-
-function compute () {
-    let num1 = parseFloat(pantallaUNO.innerHTML);
-    let num2 = parseFloat(pantallaDOS.innerHTML);
-} */
-
-// add, substract, multiply, divide <-- editar para que opere pantalla 1 vs pantalla 2
-
-function add(a, b) {
-    result = a + b;
-    document.getElementsByClassName('pantalla').innerHTML = result;
+function substract(number1, number2){
+  return number1 - number2;
 }
 
-function substract(a, b) {
-    result = a - b;
-    document.getElementsByClassName('pantalla').innerHTML = result;
+function multiply(number1, number2){
+  return number1 * number2;
 }
 
-function multiply(a, b) {
-    result = a * b;
-    document.getElementsByClassName('pantalla').innerHTML = result;
+function divide(number1, number2){
+  return number1 / number2;
 }
 
-function divide(a, b) {
-    result = a / b;
-    document.getElementsByClassName('pantalla').innerHTML = result;
+function operate(){
+  switch(operator){
+    case "+":
+      number3 = add(number1, number2);
+      number3 = Math.round(number3*100)/100;
+      break;
+    case "-":
+      number3 = subtract(number1, number2);
+      number3 = Math.round(number3*100)/100;
+      break;
+    case "*":
+      number3 = multiply(number1, number2);
+      number3 = Math.round(number3*100)/100;
+      break;
+    case "/":
+      number3 = divide(number1, number2);
+      number3 = Math.round(number3*100)/100;
+      break;
+  }
+  pantallaTop.textContent = number1 + " " + operator + " " + number2 + " =";
+  pantallaBottom.textContent = number3;
 }
 
-// operate (takes operator and 2 numbers and calls function above)
+// botones
 
-function operate (a, b) {
-    
+function button1Clicked(){     
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 1;        
+    pantallaBottom.textContent = number1;
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 1;        
+    pantallaBottom.textContent = number2;
+  }
 }
 
-// botones de borrar
+function button2Clicked(){      
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 2;        
+    pantallaBottom.textContent = number1;
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 2;        
+    pantallaBottom.textContent = number2;
+  }
+}
+
+function button3Clicked(){
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 3;        
+    pantallaBottom.textContent = number1;  
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 3;        
+    pantallaBottom.textContent = number2;
+  }
+}
+
+function button4Clicked(){     
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 4;        
+    pantallaBottom.textContent = number1;            
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 4;        
+    pantallaBottom.textContent = number2;            
+  }
+}
+
+function button5Clicked(){    
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 5;        
+    pantallaBottom.textContent = number1;     
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 5;        
+    pantallaBottom.textContent = number2;      
+  }
+}
+
+function button6Clicked(){
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 6;        
+    pantallaBottom.textContent = number1;            
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 6;        
+    pantallaBottom.textContent = number2;            
+  }
+}
+
+function button7Clicked(){
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 7;        
+    pantallaBottom.textContent = number1;            
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 7;        
+    pantallaBottom.textContent = number2;            
+  }
+}
+
+function button8Clicked(){   
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 8;        
+    pantallaBottom.textContent = number1;            
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 8;        
+    pantallaBottom.textContent = number2;            
+  }
+}
+
+function button9Clicked(){
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 9;        
+    pantallaBottom.textContent = number1;            
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 9;        
+    pantallaBottom.textContent = number2;            
+  }
+}
+
+function button0Clicked(){     
+  if (operatorClicked == false && number1.length <= 8){
+    number1 += 0;        
+    pantallaBottom.textContent = number1;            
+  }
+  else if (operatorClicked == true && number2.length <= 8){
+    number2 += 0;        
+    pantallaBottom.textContent = number2;            
+  }
+}
+
+function buttonAddClicked(){
+  operatorUsed("+")
+}
+
+function buttonSubtractClicked(){
+  operatorUsed("-")
+}
+
+function buttonMultiplyClicked(){
+  operatorUsed("*")
+}
+
+function buttonDivideClicked(){
+  operatorUsed("/")
+}
+
+function buttonEqualsClicked(){
+  if(number2 == 0 && operator == "/"){
+    pantallaTop.textContent = "Dividir por 0?";
+    pantallaBottom.textContent = "Pillin ;)";
+    number1 = "";
+    number2 = ""
+    number3 = "";
+    operatorClicked = false;
+  }
+  else if(number2 != ""){
+    operate()       
+  }
+}
+
+function buttonPointClicked(){
+  if (operatorClicked == false){
+    if (number1.includes(".") != true){
+      number1 += ".";        
+      pantallaBottom.textContent = number1;                
+    }
+  }
+  else if (operatorClicked == true){
+    if (number2.includes(".") != true){
+      number2 += ".";        
+      pantallaBottom.textContent = number2;                
+    }            
+  }
+}
+
+function buttonNegativeClicked(){
+  let negative = "-";
+  if (operatorClicked == false){   
+    if (number1.includes("-") != true){                
+      number1 = negative += number1;
+      pantallaBottom.textContent = number1;
+    }
+    else if (number1.includes("-") == true){
+      number1 = number1.substring(1);
+      pantallaBottom.textContent = number1;
+    }           
+  }
+  else if (operatorClicked == true){
+    if (number2.includes("-") != true){
+      number2 = negative += number2;
+      pantallaBottom.textContent = number2;
+    }
+    else if (number2.includes("-") == true){
+      number2 = number2.substring(1);
+      pantallaBottom.textContent = number2;
+    }       
+  }       
+}
+
+function ce(){
+  pantallaTop.textContent = "";
+  pantallaBottom.textContent = "";
+  let number1 = "";
+  let number2 = "";
+  let number3 = "";
+  let operatorClicked = false;
+}
